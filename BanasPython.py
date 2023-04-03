989d513d77057a37245b2016fdb512abcc871cb1
Faraz Yusufi <faraz.yusufi@siriusxm.com>
2023-04-03 17:04:41 -0400

    more stuff

--- BanasPython.py
+++ BanasPython.py
@@ -304,4 +304,39 @@ print(s1.symmetric_difference(s2))
 print(s1.difference(s2))
 s3.clear
 print (s3)
-s4 = frozenset(["Paul", 7])
+s4 = frozenset(["Paul", 7]) 
+
+
+def get_sum(num1: int = 1, num2: int = 1 ):
+    return num1 +num2 
+
+print(get_sum(5,4))
+def get_sum2(*args):
+    sum = 0 
+    for arg in args:
+        sum += arg
+    return sum
+print(get_sum2(1,2,3,4))
+
+def next_2(num):
+    return num + 1, num + 2
+
+i1, i2 = next_2(5) 
+print(i1, i2)
+
+def mult_by(num):
+    return lambda x: x * num 
+print("3*5=", (mult_by(3)(5)))
+
+def mult_list(list, func):
+    for x in list:
+        print(func(x))
+
+def mult_list(list, func):
+    for x in list:
+        print(func(x))
+mult_by_4 = mult_by(4)
+mult_list(list(range(0, 5)), mult_by_4)
+
+power_list = [lambda x: x ** 2,
+              lambda x: x ** 3]
