6b0aff08b7bcb4a8fc51841d042493d7be792949
Faraz Yusufi <faraz.yusufi@siriusxm.com>
2023-04-05 16:46:38 -0400

    multithreading

--- multithreading.py
+++ multithreading.py
@@ -7,14 +7,14 @@ import time
 
 def execute_thread(i):
     
-    print("Thread {} sleeps at {}".format(i, time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())))
+    print("Thread {} sleeps at {}\n".format(i, time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())), "\n")
     rand_sleep_time = random.randint(1,5)
     time.sleep(rand_sleep_time)
-    print("Thread {} stops sleeping at {}".format(i, time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())))
+    print("Thread {} stops sleeping at {}".format(i, time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())) , "\n")
 
 
 for i in range(10):
     thread = threading.Thread(target=execute_thread, args=(i,))
     thread.start()
-    print("Active Thread", threading.activeCount())
-    print("Thread Objects:", threading.enumerate())
+    print("Active Thread:", threading.active_count(),"\n")
+    #print("Thread Objects:", threading.enumerate())
